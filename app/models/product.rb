fe30a08814d4fee73205571676387361ea128189
Michael Lulich <michael.lulich@gmail.com>
2018-07-01 11:43:26 -0500

    added methods to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def is_discounted
+    if price <= 20
+      discounted = true
+    else 
+      discounted = false
+    end
+  end
+
+  def tax
+    tax_val = price * 0.09
+  end
+
+  def total
+    total = price + tax
+  end
 end
